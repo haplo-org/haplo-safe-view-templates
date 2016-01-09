2add71656a9a05438397cbbecc97e94e52fccbd9
Ben Summers <ben.summers@oneis.co.uk>
2016-01-09 09:09:56 +0000

    Apply license headers

--- src/main/java/org/haplo/template/html/DeferredRender.java
+++ src/main/java/org/haplo/template/html/DeferredRender.java
@@ -1,3 +1,9 @@
+/* Haplo Safe View Templates                          http://haplo.org
+ * (c) Haplo Services Ltd 2015 - 2016    http://www.haplo-services.com
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.         */
+
 package org.haplo.template.html;
 
 public interface DeferredRender {
